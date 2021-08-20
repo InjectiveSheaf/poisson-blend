4ba2328c5a1e1334e30bd9ffad5264d70f4caed9
Егор Плужников <pluzhnikov.egor@student.msu.ru>
2021-08-20 08:24:09 +0300

    норм выводится, исправляем копирование и вставку

--- main.cpp
+++ main.cpp
@@ -3,27 +3,27 @@
 #include <string>
 #include <algorithm>
 
-cv::Mat hfitted_output(std::vector<cv::Mat> &images){
+cv::Mat hfit(std::vector<cv::Mat> &images){
         const std::vector<cv::Mat>::iterator min_img = 
         std::min_element(images.begin(), images.end(),
             [](const cv::Mat& im1, const cv::Mat& im2){return im1.size[0] < im2.size[0];}
         );
 
-        const auto resize = [](cv::Mat& im){
-            cv::resize(im, im, cv::Size(150,150));
+        const auto resize = [&min_img](cv::Mat& im){
+            cv::resize(im, im, min_img->size());
         };
 
         std::for_each(images.begin(), images.end(), resize);
 
         cv::Mat result = *images.begin();
 
-        for(auto it = images.begin(); it != images.end(); ++it){
+        for(auto it = images.begin()+1; it != images.end(); ++it){
             cv::hconcat(result, *it, result);
         }
         return result;
 }
 
-class Interface(){
+class Interface{
 private:
     cv::Mat foreground;
     cv::Mat background;
@@ -35,7 +35,7 @@ public:
         foreground = fg.clone();
         background = bg.clone();
         dispForeground = fg.clone();
-        background = bg.clone();
+        dispBackground = bg.clone();
         std::cout << "help: " << std::endl << " s = select and clone, r = return " << std::endl;
         cv::namedWindow("window");
     }
@@ -46,14 +46,14 @@ public:
             if(key == 27) return;
             if(key == 115) selectClone();
             if(key == 114) bringBack();
-            std::vector<cv::Mat> images = {dispForeground, dispBackground};
-            cv::imshow("window",hfitted_output(images));
+            std::vector<cv::Mat> images{dispForeground, dispBackground};
+            cv::imshow("window", hfit(images));
         }
     }
 
     void aim(cv::Mat& disp, cv::Mat& ground){
-        std::cout << "input coords: x in (0," << ground.shape[1] << "), y in (0," 
-                << ground.shape[0] << ")" << std::endl;
+        std::cout << "input coords: x in (0," << ground.size[1] << "), y in (0," 
+                << ground.size[0] << ")" << std::endl;
         disp = ground.clone();
         cv::rectangle(disp, cv::Rect(cv::Point(x1,y1), cv::Point(x2,y2)), (0,255,0));
     }
@@ -65,28 +65,24 @@ public:
         cv::resize(temp, temp, cv::Size(x2-x1, y2-y1));
         temp.copyTo(dispBackground(cv::Rect(cv::Point(x1,y1),cv::Point(x2,y2))));
     }
-}
+
+    void bringBack(){
+        std::cout << "bringing all back" << std::endl;
+        dispForeground = foreground.clone();
+        dispBackground = background.clone();
+    }
+};
 
 int main(int argc, const char** argv) {
     cv::Mat fg_img = cv::imread("foreground_im.jpg", cv::IMREAD_ANYCOLOR);
     cv::Mat bg_img = cv::imread("background_im.jpg", cv::IMREAD_ANYCOLOR);
-    cv::Mat temp;
     if (bg_img.empty() || fg_img.empty()) {
         std::cout << "Error: Image cannot be loaded." << std::endl;
         system("pause");
         return -1;
     }
-    temp = fg_img.clone();
-    const std::string winName = "Image Window";
-    cv::namedWindow(winName);
-
-    while(true){
-        if(drawing_box) draw_box(temp,box);
-        std::vector<cv::Mat> images{temp, bg_img};
-        cv::imshow(winName,hfitted_output(images));
-        if (cv::waitKey(15) == 27) break;
-    }
-
+    Interface I(fg_img, bg_img);
+    I.startHandler();
     return 0;
 }
 
