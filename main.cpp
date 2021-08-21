4c8fec43b99afce1cac7030a1d91bb65d6cff54c
Егор Плужников <pluzhnikov.egor@student.msu.ru>
2021-08-21 09:30:43 +0300

    делаем так, чтобы eigen использовался только для решения системы

--- main.cpp
+++ main.cpp
@@ -25,6 +25,8 @@ cv::Mat hfit(std::vector<cv::Mat> &images){
         }
         return result;
 }
+
+
 /* Имеем f(x,y) - искомую функцию на Omega и f*(x,y) - известную функцию на S
  * Помимо этого знаем градиент gradFx(x,y) = df/dx + df/dy 
  * Пока что работаем в условии того, что включение Omega \in S нестрогое,
@@ -35,11 +37,13 @@ cv::Mat hfit(std::vector<cv::Mat> &images){
  * тогда, составляем матрицу из векторов, а значения справа - 
  * суммируя результаты функций getRightSum и getGradient для каждой строки.
  */
+/*
 template<typename Derived>
 class Poisson{ // Для одного канала, в интерфейсе получим для всех трёх и сложим
 private:
-    Eigen::Matrix<typename Derived::cellType, Derived::Rows, Derived::Columns> Omega; // Omega \in S 
-    Eigen::Matrix<typename Derived::cellType, Derived::Rows, Derived::Columns> S; 
+    // Eigen::Matrix<typename Derived::cellType, Derived::Rows, Derived::Columns> Omega; // Omega \in S 
+    // Eigen::Matrix<typename Derived::cellType, Derived::Rows, Derived::Columns> S; 
+    Eigen::MatrixXd Omega, S;
     bool isNeighbour(cv::Point p, cv::Point q){
         int l1_norm = std::abs(p.x - q.x) + std::abs(p.y - q.y);
         return l1_norm == 1;
@@ -50,21 +54,24 @@ private:
         return grad_x + grad_y;
     }
 public:
-/* Для того, чтобы пользоваться в дальнейшем произвольными формами, а не только прямоугольниками
- * Используем представление через маску:
- * 1. Omega = 0 вне области вставки
- * 2. Для удобства предполагаем, что dim(Omega) = dim(S).
- */ 
+ // Для того, чтобы пользоваться в дальнейшем произвольными формами, а не только прямоугольниками
+ // Используем представление через маску:
+ // 1. Omega = 0 вне области вставки
+ // 2. Для удобства предполагаем, что dim(Omega) = dim(S).
+ // 
     Poisson();
     void loadMatrices(Eigen::MatrixBase<Derived> & _Omega, Eigen::MatrixBase<Derived> & _S){
+    //void loadMatrices(Eigen::MatrixXd & _Omega, Eigen::MatrixXd & _S){
         Omega = _Omega;
         S = _S;
     }
 
     Eigen::Matrix<typename Derived::cellType, Derived::Rows, Derived::Columns> solve(){ 
+    // Eigen::MatrixXd solve(){
         int k = Omega.size() - std::count(Omega.reshaped().begin(),Omega.reshaped().end(), 0); // количество пикселей
-        Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> A;
-        Eigen::Matrix<double,Eigen::Dynamic,1> b;
+        // int k = (Omega.array() != 0).count();
+        Eigen::MatrixXd A;
+        Eigen::VectorXd b;
         std::unordered_map<cv::Point, double> f; // f: Omega -> R
         
         // Заполняем наше отображение
@@ -84,6 +91,7 @@ public:
                 if(q_key == p_key) lhs << 4; // Пиксель, у которого мы смотрим окрестность
                 else lhs << 0; // Пиксели вне окрестности p
             }
+            A << lhs;
             // Считаем правую часть
             std::vector<cv::Point> neighbours = {(0,1),(1,0),(0,-1),(-1,0)};
             for(const auto nbs : neighbours){
@@ -92,7 +100,37 @@ public:
             rhs += grad(p_key);
         }
     }
-};
+}; 
+*/
+
+class PoissonSolver{
+private:
+    cv::Mat Omega;
+    cv::Mat S;
+    Eigen::MatrixXd A;
+    Eigen::VectorXd b;
+    bool isNeighbour(cv::Point p, cv::Point q){
+        int l1_norm = std::abs(p.x - q.x) + std::abs(p.y - q.y);
+        return l1_norm == 1;
+    }
+    double gradOmega(cv::Point p){
+        double grad_x = Omega.at(p.x+1, p.y) - Omega(p.x-1, p.y);
+        double grad_y = Omega.at(p.x, p.y+1) - Omega(p.x, p.y-1);
+        return (grad_x + grad_y)/2;
+    }
+public:
+    PoissonSolver(cv::Mat & overlayingImage, cv::Mat & baseImage){
+        Omega = overlayingImage;
+        S = baseImage;
+    }
+    void updateMatrices(cv::Mat & overlayingImage, cv::Mat & baseImage){
+        Omega = _Omega;
+        S = _S;
+    }
+    cv::Mat ResultImage(){
+       int k = Omega.rows() * Omega.cols() - std::count(const auto x : Omega, 0);
+    }
+}
 
 class Interface{
 private:
@@ -107,7 +145,7 @@ public:
         background = bg.clone();
         dispForeground = fg.clone();
         dispBackground = bg.clone();
-        std::cout << "help: " << std::endl << " s = select and clone, r = return " << std::endl;
+        std::cout << "help: " << std::endl << " s = select and clone, p = poisson clone, r = return " << std::endl;
         cv::namedWindow("window");
     }
 
@@ -115,6 +153,7 @@ public:
         while(true){
             unsigned int key = cv::waitKey(15);
             if(key == 27) return;
+            if(key == 112) poissonClone();
             if(key == 115) selectClone();
             if(key == 114) bringBack();
             std::vector<cv::Mat> images{dispForeground, dispBackground};
@@ -139,8 +178,26 @@ public:
     }
 
     void poissonClone(){
+        // Подготавливаем область для вырезания
         aim(dispForeground, foreground);
         cv::Mat temp = foreground(cv::Rect(cv::Point(x1,y1),cv::Point(x2,y2)));
+        aim(dispBackground, background);
+        cv::resize(temp,temp, cv::Size(x2-x1, y2-y1));
+        cv::Mat mask(foreground.size(),foreground.type(),cv::Scalar(0,0,0));
+        temp.copyTo(mask(cv::Rect(cv::Point(x1,y1),cv::Point(x2,y2))));
+        // Имеем маску с изображением, теперь разобьем всё на каналы 
+        std::vector<cv::Mat> maskChannels(3);
+        std::vector<cv::Mat> bgChannels(3);
+        cv::split(mask,maskChannels);
+        cv::split(background,bgChannels);
+        // И поканально пропустим в алгоритм
+        std::vector<cv::Mat> resChannels(3);
+        for(int i = 0; i < 3; i++){
+            cv::cv2eigen(maskChannels[i], Omega);
+            cv::cv2eigen(bgChannels[i], S);
+            Poisson<Eigen::Matrix<double, rows, cols>> *SD = new Poisson();
+            Poisson->loadMatrices(Omega, S);
+        }
     }
 
     void bringBack(){
