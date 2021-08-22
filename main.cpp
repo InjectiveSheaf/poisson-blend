c12f878bf22fb29020b29e493de376dd589ebd19
Егор Плужников <pluzhnikov.egor@student.msu.ru>
2021-08-23 02:19:53 +0300

    нечто рабочее с примерами

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include "opencv2/opencv.hpp"
 #include "eigen3/Eigen/Sparse"
+#include <random>
 
 cv::Mat hfit(std::vector<cv::Mat> &images){
         const std::vector<cv::Mat>::iterator fitImg = 
@@ -42,7 +43,7 @@ private:
     cv::Mat S;
     Eigen::SparseMatrix<double> A;
     Eigen::VectorXd b;
-
+    std::vector<cv::Point> neighbours;
     struct PointComparator{
         bool operator () (const cv::Point& a, const cv::Point& b) const{ 
             return (a.x < b.x) || (a.x == b.x && a.y < b.y);
@@ -52,53 +53,51 @@ private:
     std::map<cv::Point, uchar, PointComparator> f;
     typedef std::map<cv::Point, uchar, PointComparator>::iterator pixelMapIter;
 
-    double gradOmega(cv::Point p){
+    double sumofFieldProj(cv::Point p){
         return (Omega.at<uchar>(p + cv::Point(1,0)) + Omega.at<uchar>(p + cv::Point(-1,0))
                 + Omega.at<uchar>(p + cv::Point(0,1)) + Omega.at<uchar>(p + cv::Point(0,-1)) 
-                )-4*Omega.at<uchar>(p);
+                -4*Omega.at<uchar>(p));
     }
+
 public:
+    Poisson(){
+        neighbours = {cv::Point(0,1), cv::Point(1,0), cv::Point(0,-1), cv::Point(-1,0)};
+    }
+
     void updateMatrices(cv::Mat & overlayingImage, cv::Mat & baseImage){
         f.clear();
         Omega = overlayingImage.clone();
         S = baseImage.clone();
-        for(int y = 0; y < Omega.rows; ++y){
-            for(int x = 0; x < Omega.cols; ++x){
-                cv::Point p(x,y);
-                if(Omega.at<uchar>(p) != 0){
-                    f[p] = Omega.at<uchar>(p);
-                }
-            }
-        } 
+        for(int y = 0; y < Omega.rows; ++y)
+            for(int x = 0; x < Omega.cols; ++x)
+                if(Omega.at<uchar>(cv::Point(x,y)) != 0)
+                    f[cv::Point(x,y)] = Omega.at<uchar>(cv::Point(x,y));
         A.resize(f.size(), f.size());
         b.resize(f.size());
     }
-    
+
 /* Cоставляем уравнения:
  * Итерируем по всем пикселям p в Map f.
  * Задаем коэффициенты разреженной матрицы A и вектор-столбца b.
  * Коэффициенты: -1 в пересечении N и Omega, и 4 для p 
  */
-    void buildSLE(){    
+    void buildSLE(){
         for(pixelMapIter p = f.begin(); p != f.end(); ++p){
             int p_index = std::distance<pixelMapIter> (f.begin(), p);
             int b_temp = 0;
-            
-            A.insert(p_index, p_index) = 4; // рассматриваемый пиксель
-            std::vector<cv::Point> neighbours{cv::Point(0,1), cv::Point(1,0), cv::Point(0,-1), cv::Point(-1,0)};
-            // задаем коэффициенты для пересечении окрестности и внутренности/границы множества
-            for(const auto n : neighbours){
-                pixelMapIter q = f.find(p->first + n);
-                if(q != f.end()){ 
-                    int q_index = std::distance<pixelMapIter> (f.begin(), q);
-                    A.insert(p_index, q_index) = -1;
-                }
-                else b_temp += S.at<uchar>(p->first + n); // если нет, то это граница Omega
+
+    /* задаем коэффициенты для пересечении окрестности и внутренности/границы множества 
+     * Если пиксель содержится в f, то он принадлежит внутренности Omega, иначе - границе */
+            A.insert(p_index, p_index) = 4; 
+            for(auto n : neighbours){
+                pixelMapIter q = f.find(p->first + n); // здесь можно ускорить
+                int q_index = std::distance<pixelMapIter> (f.begin(), q);
+                if(q != f.end()) A.insert(p_index, q_index) = -1;
+                else b_temp += S.at<uchar>(p->first + n);
             }
 
-            b_temp += gradOmega(p->first);
+            b_temp += sumofFieldProj(p->first)/4; // почему 4?
             b(p_index) = b_temp;
-            // std::cout << "SLE building: " << p_dist << " of " << f.size() << std::endl;
         }
         std::cout << "SLE has been builded in a matrix form" << std::endl;
     }
@@ -106,19 +105,13 @@ public:
     cv::Mat getResult(){
         Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> LDLT(A);
         Eigen::VectorXd x = LDLT.solve(b);
-        std::cout << *(std::min_element(x.data(), x.data()+x.size())) << " <- min, max ->" << *(std::max_element(x.data(),x.data()+x.size())) << std::endl;
-        cv::Mat result(S.size(), S.type(), cv::Scalar(0,0,0));
+        std::cout << "Solved: min->" << *(std::min_element(x.data(), x.data()+x.size())) << ", max ->" 
+                << *(std::max_element(x.data(),x.data()+x.size())) << std::endl;
+        cv::Mat result = S.clone();
         for(pixelMapIter p = f.begin(); p != f.end(); ++p){
-            result.at<uchar>(p->first) = uchar(x(std::distance<pixelMapIter> (f.begin(), p)));
-        }
-        for(int y = 0; y < result.rows; ++y){
-            for(int x = 0; x < result.cols; ++x){
-                cv::Point p(x,y);
-                if(Omega.at<uchar>(p) == 0){
-                    result.at<uchar>(p) = S.at<uchar>(p);
-                }
-            }
-        }
+            int color = x(std::distance<pixelMapIter> (f.begin(), p));
+            result.at<uchar>(p->first) = color;
+        } 
         return result;
     }
 };
@@ -129,6 +122,7 @@ private:
     cv::Mat background;
     cv::Mat dispForeground;
     cv::Mat dispBackground;
+    cv::Mat fittedDisp;
     int x1, y1, x2, y2;
 public:
     Interface(cv::Mat& fg, cv::Mat& bg){
@@ -137,23 +131,29 @@ public:
         dispForeground = fg.clone();
         dispBackground = bg.clone();
         std::cout << "help: " << std::endl << "s = select and clone, p = poisson clone, r = return, w = write." << std::endl;
-        cv::namedWindow("window");
+        cv::namedWindow("Poisson Blender");
     }
 
     void startHandler(){
         while(true){
+            hfitImages();
+            cv::imshow("Poisson Blender", fittedDisp);
+                    
             unsigned int key = cv::waitKey(15);
-            if(key == 27) return;
-            if(key == 112) poissonClone();
-            if(key == 115) selectClone();
-            if(key == 114) bringBack();
-            if(key == 119) writeImage();
-            std::vector<cv::Mat> images{dispForeground, dispBackground};
-            cv::imshow("window", hfit(images));
+            if(key == 27)     return;
+            if(key == 112)    poissonClone();
+            if(key == 115)    simpleClone();
+            if(key == 114)    bringBack();
+            if(key == 119)    cv::imwrite("result.jpg", fittedDisp);
         }
     }
 
-    void aim(cv::Mat& disp, cv::Mat& ground){
+    void hfitImages(){
+            std::vector<cv::Mat> dispVec = {dispForeground, dispBackground};
+            fittedDisp = hfit(dispVec);
+    }
+
+    void selectRectangle(cv::Mat& disp, cv::Mat& ground){
         std::cout << "input coords: x in (0," << ground.size[1] << "), y in (0," 
                 << ground.size[0] << ")" << std::endl;
         std::cin >> x1 >> y1 >> x2 >> y2;
@@ -161,37 +161,42 @@ public:
         cv::rectangle(disp, cv::Rect(cv::Point(x1,y1), cv::Point(x2,y2)), cv::Scalar(0,255,0));
     }
 
-    void selectClone(){
-        aim(dispForeground, foreground);
+    void simpleClone(){
+        selectRectangle(dispForeground, foreground);
         cv::Mat temp = foreground(cv::Rect(cv::Point(x1,y1),cv::Point(x2,y2)));
-        aim(dispBackground, background);
+        selectRectangle(dispBackground, background);
         cv::resize(temp, temp, cv::Size(x2-x1, y2-y1));
         temp.copyTo(dispBackground(cv::Rect(cv::Point(x1,y1),cv::Point(x2,y2))));
     }
 
     void poissonClone(){
         // Подготавливаем область для вырезания
-        aim(dispForeground, foreground);
+        selectRectangle(dispForeground, foreground);
         cv::Mat temp = foreground(cv::Rect(cv::Point(x1,y1),cv::Point(x2,y2)));
-        aim(dispBackground, background);
+        
+        selectRectangle(dispBackground, background);
         cv::resize(temp,temp, cv::Size(x2-x1, y2-y1));
+
         cv::Mat mask(background.size(),background.type(),cv::Scalar(0,0,0));
         temp.copyTo(mask(cv::Rect(cv::Point(x1,y1),cv::Point(x2,y2))));
+        
         // Имеем маску с изображением, теперь разобьем всё на каналы 
+        
         std::vector<cv::Mat> maskChannels(3);
         std::vector<cv::Mat> bgChannels(3);
         cv::split(mask,maskChannels);
         cv::split(background,bgChannels);
+        
         // И поканально пропустим в алгоритм
+        
         cv::Mat result;
         std::vector<cv::Mat> resChannels(3);
         Poisson* SimeonDenis = new Poisson();
-
         for(int i = 0; i < 3; i++){
             SimeonDenis->updateMatrices(maskChannels[i], bgChannels[i]);
             SimeonDenis->buildSLE();
-            resChannels[i] = SimeonDenis->getResult().clone();
-            dispBackground = result.clone();
+            resChannels[i] = SimeonDenis->getResult().clone();      
+            // cv::imwrite("res"+std::to_string(i)+".jpg", resChannels[i]);
         }
         cv::merge(resChannels,result);
         dispBackground = result.clone();
@@ -201,10 +206,6 @@ public:
         std::cout << "bringing all back" << std::endl;
         dispForeground = foreground.clone();
         dispBackground = background.clone();
-    }
-
-    void writeImage(){
-        cv::imwrite("result.jpg", dispBackground);
     }
 };
 
