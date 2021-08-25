dcb1a076841f8e2168de5ad13ede015fd796783a
Егор Плужников <pluzhnikov.egor@student.msu.ru>
2021-08-25 11:39:50 +0300

    Release

--- interface.cpp
+++ interface.cpp
@@ -1,16 +1,27 @@
 #include "interface.h"
 
+// Конструктор копирует изображения, с которыми дальше работает класс
+
 Interface::Interface(cv::Mat& fg, cv::Mat& bg){
+    if(fg.size[0] > bg.size[0] || fg.size[1] > bg.size[1]){
+        std::cout << "image sizes does not match, please try again." << std::endl 
+                  << "the dimension of overlaying image must be less than the dimension of the background"
+                  << std::endl;
+    }
     foreground = fg.clone();
     background = bg.clone();
     dispForeground = fg.clone();
     dispBackground = bg.clone();
-    std::cout << "help:" << std::endl << 
-            "press s to go to selection menu, r to bring back all pictures, ESC to exit" << std::endl;
+    std::cout << "help:" << std::endl
+              << "press s to go to the selection menu, r to bring back all pictures, ESC to exit" << std::endl;
     cv::namedWindow("Poisson Blender");
 }
 
-
+// fitFormImage используется в качестве шаблона высоты. 
+// Можно выбирать минимум/максимум заменой стандартной функции
+// Лямбда resize приводит изображения к необходимой высоте
+// Проводим это для каждого изображения с помощью for_each
+// Далее соединяем изображения одной высоты по горизонтали в цикле
 
 cv::Mat Interface::hfit(std::vector<cv::Mat> &images){
     const auto fitFormImage =
@@ -32,58 +43,79 @@ cv::Mat Interface::hfit(std::vector<cv::Mat> &images){
     return result;
 }
 
-
+// Приводим все изображение в необходимый для вывода вид и выводим:
+// Копируем изображение из шаблонного, рисуем на нём необходимым образом отмасштабированный массив точек
+// Далее создаем вектор из изображений, масштабируем с помощью описанной выше hfit и выводим
 
 void Interface::showImages(){
     dispForeground = foreground.clone();
-    for(const auto p : pointVector)
-        cv::circle( dispForeground, p * (fittedDisp.size[0] / dispForeground.size[0]), 
-                        3, cv::Scalar(0,255,0), -1 );
-       
+    for(const auto p : pointVector){
+        p *= fittedDisp.size[0] / dispForeground.size[0];
+        cv::circle( dispForeground, p, 3, cv::Scalar(0,255,0), -1 );
+    }
     std::vector<cv::Mat> dispVec = {dispForeground, dispBackground};
     fittedDisp = hfit(dispVec);
     cv::imshow("Poisson Blender", fittedDisp);
 }
 
+// Выводим информацию об используемых клавишах.
+// Показываем изображения, далее дожидаемся нажатия и делаем соответствующую операцию.
+// 114 == r, 97 == a, 100 == d в кодировке ASCII
+
 void Interface::selectPolygon(){
     std::cout << "Help:" << std::endl << "a = add point," << std::endl;
                             std::cout << "d = delete last point," << std::endl;
                             std::cout << "r = return back" << std::endl;
-    std::cout << "when the first point will be entered again, Poisson blending will start." << std::endl;
+    std::cout << "when you will enter the first point again, blending will start." << std::endl;
 
     while(true){
         showImages();
         
         unsigned int key = cv::waitKey(15);
-        if(key == 114){     // r
+
+        if(key == 114){     
             std::cout << "Getting back to menu." << std::endl; 
             return;
         }
-        if(key == 97) selectPoint(); // a
-        if(key == 100 && !pointVector.empty()) pointVector.pop_back(); // d
+
+        if(key == 97) selectPoint(); 
+
+        if(key == 100 && !pointVector.empty()) pointVector.pop_back(); 
     }
 }
 
-
+// Производим выбор точки.
+// Вводим координаты, до того момента, пока последняя введенная координата не станет равна первой
+// pointVector.size() > 2 необходим для того, чтобы можно было построить выпуклую оболочку точек
+// Рисуем на маске выпуклую оболочку и при замыкании многоугольника производим пуассоновское клонирование.
 
 void Interface::selectPoint(){
     std::cout << "Enter point coordinates: x in (0, " << foreground.size[0] << "), " 
             << "y in (0, " << foreground.size[1] << ") "<< std::endl;
+    
     int x, y;
     std::cin >> x >> y;
+   
     if(pointVector.size() > 2 && x == pointVector[0].x && y == pointVector[0].y){
         std::cout << "Attempting to realise Poisson blending:" << std::endl;
+        
         cv::Mat binaryMask(background.size(), background.type(), cv::Scalar(0,0,0));
         cv::fillConvexPoly(binaryMask, pointVector.data(), 
                         pointVector.size(), cv::Scalar(255,255,255));
+        
         poissonClone(binaryMask);
         std::cout << "Done. Press r to return to main menu." << std::endl;
         return;
     }
+
     pointVector.push_back(cv::Point(x,y));
 }
 
-
+// Оболочка для главного алгоритма: 
+// Вырезаем накладываемое изображение по трафарету binaryMask в новый массив mask
+// Разбиваем mask и background на каналы, которые будут объединяться в массив result
+// Пропускаем каждый канал через алгоритм, получаем результирующий канал итогового изображения
+// Объединяем каналы и показываем результат.
 
 void Interface::poissonClone(cv::Mat & binaryMask){
     cv::Mat mask(background.size(), background.type(),cv::Scalar(0,0,0));
@@ -107,17 +139,20 @@ void Interface::poissonClone(cv::Mat & binaryMask){
     dispBackground = result.clone();
 }
 
-
+// Собственно говоря, обработчик событий
+// Позволяет переходить в режим выбора маски по нажатию s, возвращать изображения обратно по r, 
+// Записывать результат при нажатии w и выходить по ESC
+// 27 == esc, 115 = s, 114 = r, 119 = w в кодировке ASCII
 
 void Interface::startHandler(){
     while(true){
         showImages();
         
         unsigned int key = cv::waitKey(15);
-        if(key == 27)     return;           // esc
-        if(key == 115)    selectPolygon();  // s
-        if(key == 114)    bringBack();      // r
-        if(key == 119)    cv::imwrite("result.jpg", fittedDisp); // w
+        if(key == 27)     return;           
+        if(key == 115)    selectPolygon();  
+        if(key == 114)    bringBack();      
+        if(key == 119)    cv::imwrite("result.jpg", fittedDisp); 
     }
 }
 
--- poisson.cpp
+++ poisson.cpp
@@ -1,20 +1,38 @@
 #include "poisson.h"
 
 
-float Poisson::sumofFieldProj(cv::Point p, cv::Mat& img){
+// Считаем лапласиан как поток векторного поля через данную точку, 
+// Аппроксимируя суммой частных производных первого порядка
+// (можно попробовать со вторым порядком, но вряд ли будет сильное различие)
+
+float Poisson::laplace(cv::Point p, cv::Mat& img){
     return  4*img.at<float>(p)
             - img.at<float>(p + cv::Point(1,0)) - img.at<float>(p + cv::Point(-1,0))
             - img.at<float>(p + cv::Point(0,1)) - img.at<float>(p + cv::Point(0,-1));
 }
 
+// Если сосед пикселя входит в изображение и он не пустой, т.е. входит в маску - он считается соседом.
+// Суммируем в цикле и выводим.
+
 int Poisson::neighboursCount(cv::Point p){
     int N = 0;
     for(auto n : neighbours)
-        if(((p+n).x >= 0) && ((p+n).y >= 0) && ((p+n).x < Omega.cols) && ((p+n).y < Omega.rows) 
-                        && (Omega.at<float>(p + n) != 0) ) N++;
+        if( (p+n).x >= 0         && (p+n).y >= 0         &&
+            (p+n).x < Omega.cols && (p+n).y < Omega.rows && 
+            Omega.at<float>(p + n) != 0 )   N++;
     return N;
 }
 
+// Мы используем упорядоченный map во многом потому, что он позволяет 
+// биективно сопоставить двумерную точку cv::Point и номер неизвестной в строке матрицы А
+// Взаимно-однозначное соответствие достигается благодаря:
+// 1. Одинаковому порядку обхода
+// 2. Функции distance, которая как раз является взаимно-однозначной
+// Можно использовать unordered_map, но для этого необходимо прописать хэш-функцию, вычисляемую для каждого f
+// Она тоже будет взаимно-однозначной, при этом вставка и поиск будут вычисляться за O(1), а не за O(logN)
+// (позже будет пробоваться)
+// Касательно функции: вбиваем в f только точки, входящие во внутренность множества
+// Далее запускаем построение системы
 
 void Poisson::updateMatrices(cv::Mat & overlayingImage, cv::Mat & baseImage){
     Omega = overlayingImage.clone();
@@ -28,16 +46,17 @@ void Poisson::updateMatrices(cv::Mat & overlayingImage, cv::Mat & baseImage){
             if(Omega.at<float>(p) != 0 && neighboursCount(p) == 4) f[p] = Omega.at<float>(p);
         }
     }
-    std::cout << "dgdaf";
     buildSLE();
 }
 
-
- // Cоставляем уравнения:
- // Итерируем по всем пикселям p в Map f.
- // Задаем коэффициенты разреженной матрицы A и вектор-столбца b.
- // Коэффициенты: -1 в пересечении N и Omega, и 4 для p 
-
+// Система составляется следующим образом
+// Итерируем по всем пикселям f_p в Map f.
+// Задаем коэффициенты разреженной матрицы A: 
+// При f_p стоит 4, при его соседях, если они принадлежат Omega, -1, то есть задаётся аппроксимация лапласиана
+// В p-том элементе вектор-столбце b сумма по всем граничным значениям его соседей на границе
+// Пройдя всех соседей, добавляем в b_p известное значение лапласиана
+// И решаем в численном виде уравнение Пуассона для каждого пикселя: \nabla_d (f_p) - \nabla_b (f) = Sum
+// Где nabla_d - дискретный, а nabla_b - известный граничный лапласиан, а Sum - сумма значений на границе
 
 
 void Poisson::buildSLE(){
@@ -48,7 +67,6 @@ void Poisson::buildSLE(){
     for(pixelMapIterator f_p = f.begin(); f_p != f.end(); f_p++){
         int p = std::distance<pixelMapIterator> (f.begin(), f_p);
         A.insert(p, p) = 4;
-        b(p) += sumofFieldProj(f_p->first, Omega);
         
         for(auto n : neighbours){ 
             if(neighboursCount(f_p->first + n) == 4){
@@ -58,24 +76,38 @@ void Poisson::buildSLE(){
             }
             else b(p) += S.at<float>(f_p->first + n);
         }
+
+        b(p) += laplace(f_p->first, Omega);
     }
 
     std::cout << "SLE has been builded in a matrix form" << std::endl;
 }
 
+// Каждая строка матрицы A будет иметь вид (0,...0,-1,0,...,0,-1,4,-1,0,...0,-1,0,...)
+// Т.е., A будет очень сильно разреженной. 
+// Библиотека Eigen предоставляет класс солверов для систем с разреженными матрицами.
+// В качестве прямых методов решения - LL^T, LDL^T, QR и LU - разложения.
+// LL^T (Холецкий) использует большое количество квадратных корней => сильная потеря в точности/скорости
+// QR - разложение вращениями звучит очень хорошо, так как нам придётся занулять очень мало компонент векторов.
+// LU - разложение будет проходить по циклу большое количество раз, гораздо большее, чем количество вращений.
+// LDL^T - обладает очень хорошей устойчивостью для больших матриц, однако будет считаться медленней, чем QR.
+
+// Из итогов - отбрасываем LL^T и LU
+// Однако нужно протестировать на производительность и устойчивость относительно друг друга QR и LDL^T
+// Сейчас используется LDL^T в предположении того, что на небольших данных производительность будет сравнима, а перенасыщение пикселей - меньше.
 
 
 cv::Mat Poisson::getResult(){
     Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> LDLT(A);
     Eigen::VectorXd x = LDLT.solve(b);
     std::cout << "Solved: min->" << *(std::min_element(x.data(), x.data()+x.size())) << ", max ->" 
-            << *(std::max_element(x.data(),x.data()+x.size())) << std::endl;
+                                 << *(std::max_element(x.data(),x.data()+x.size())) << std::endl;
     
     cv::Mat result = S.clone();
     for(pixelMapIterator f_p = f.begin(); f_p != f.end(); ++f_p){
         int p = std::distance<pixelMapIterator> (f.begin(), f_p);
         float color = x(p);
-        if (neighboursCount(f_p->first) != 4) continue;
+        if (neighboursCount(f_p->first) != 4) continue; // Пиксели на границе мы берем из S
         if (color < 0) color = 0;
         result.at<float>(f_p->first) = color;
     } 
